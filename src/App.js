3317b9d60fb6c22585a39b72bc4e8b5c33491529
eniolakura <enybeats76@gmail.com>
2022-10-31 18:02:48 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,53 @@
 import logo from './logo.svg';
 import './App.css';
+import profile from './profile__img.png';
+import slack from './slack.png';
+import github from './github.png';
+import zuri from './zuriLogo.png';
+import Ig4 from './I4G.png';
+import text from './Footer_text.png';
+import share from './share_button.png';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+    <header id='head'>
+      {/* <p class='slack_username'>Eniola Kuranga</p> */}
+      <img id='profile__img' src={ profile } />
+      <img />
+      <h2>Eniola Kuranga</h2>
+      <img src={ share } />
+    </header>
+    <div className='buttons'>
+      <a href='https://twitter.com/bat_syyyy'>
+        Twitter link
+      </a>
+      <a id='btn__zuri' href='https://training.zuri.team/' alt='zuri team'>
+           Zuri Team
+      </a>
+      <a id='books' href='http://books.zuri.team/' alt='zuri books'>
+          Zuri Books
+      </a>
+      <a id='book__python' href='https://books.zuri.team/python-for-beginners?ref_id=EniolaKuranga' alt='python books'>
+          Python Books
+      </a>
+      <a id='pitch' href='https://background.zuri.team' alt='background'>
+          Background Check for coders
+      </a>
+      <a id='book_design' href="https://books.zuri.team/design-rules" alt='design'>
+          Design Books
+      </a>
+      <div id='social-icons'>
+        <img src={ slack } id='slack' onClick={""}/>
+        <img src={ github } />
+      </div>
+      <footer id='logos'>
+        <img src={ zuri } />
+        <img src={ text } />
+        <img src={ Ig4 } />
+      </footer>
     </div>
+  </div>
   );
 }
 
